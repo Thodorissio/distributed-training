9c5d69fb477a73b3753d31819be4b5a59a182509
jimbou <el17072@mail.ntua.gr>
2022-09-08 14:33:13 +0300

    create test folder from tutorial to test functionality of vms

---
+++ python_scripts/densenet/test/run_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Training test model in $1 distirbuted machines"
+
+
+/home/user/distirbuted-training/test/script_master.sh $1  &
+ssh user@192.168.1.1 'bash -s' < /home/user/distirbuted-training/test/script_1.sh $1  & #tsekare ta paths
+ssh user@192.168.1.3 'bash -s' < /home/user/distirbuted-training/test/script_3.sh $1  &
+
+wait
+echo "All 3 complete"
---
+++ python_scripts/densenet/test/script_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $1 == "1" ]; then
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22"]}, "task": {"index": 0, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/test/worker.py
+  echo "Running script for test model only on master vm (who is me)"
+elif [ $1 == "2" ];
+then
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 0, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/test/worker.py  
+  echo "Running script for test model on master vm (who is me) and on vm 1"
+elif [ $1 == "3" ];
+then
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 0, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/test/worker.py
+  echo "Running script for test model on all 3 vms , including myself"
+fi
