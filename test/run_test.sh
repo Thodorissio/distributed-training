d7717fbe977394a55162c496da942707050e87c7
jimbou <el17072@mail.ntua.gr>
2022-09-08 16:26:59 +0300

    changed nothing

--- test/run_test.sh
+++ test/run_test.sh
@@ -7,5 +7,5 @@ echo "Training test model in $1 distirbuted machines"
 ssh user@192.168.1.1 'bash -s' < /home/user/distirbuted-training/test/script_1.sh $1  & #tsekare ta paths
 ssh user@192.168.1.3 'bash -s' < /home/user/distirbuted-training/test/script_3.sh $1  &
 
-wait
+wait #1
 echo "All 3 complete"
