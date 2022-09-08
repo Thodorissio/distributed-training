45b9f854e23f7f43b10b88d3b22fd6dd4fee8550
Thodorissio <thodorissiozos@gmail.com>
2022-09-08 16:56:24 +0300

    typos fix

--- bash_scripts/script_3.sh
+++ bash_scripts/script_3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $2 == "1" ]; then
-  #TF_CONFIG='{"cluster": {"worker": ["83.212.80.22"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/python_scripts/$1.py
+  #TF_CONFIG='{"cluster": {"worker": ["83.212.80.22"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/python_scripts/$1.py
   echo "Running script for $1 model only on master vm ,not here"
 elif [ $2 == "2" ];
 then
-  #TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/python_scripts/$1.py  
+  #TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/python_scripts/$1.py  
   echo "Running script for $1 model on master vm and on vm 1 ,not here"
 elif [ $2 == "3" ];
 then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distirbuted-training/python_scripts/$1.py  
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/python_scripts/$1.py  
   echo "Running script for $1 model on all 3 vms , including here"
 fi
--- test/run_test.sh
+++ test/run_test.sh
@@ -3,9 +3,9 @@
 echo "Training test model in $1 distirbuted machines"
 
 
-/home/user/distirbuted-training/test/script_master.sh $1  &
-ssh user@192.168.1.1 'bash -s' < /home/user/distirbuted-training/test/script_1.sh $1  & #tsekare ta paths
-ssh user@192.168.1.3 'bash -s' < /home/user/distirbuted-training/test/script_3.sh $1  &
+/home/user/distributed-training/test/script_master.sh $1  &
+ssh user@192.168.1.1 'bash -s' < /home/user/distributed-training/test/script_1.sh $1  & #tsekare ta paths
+ssh user@192.168.1.3 'bash -s' < /home/user/distributed-training/test/script_3.sh $1  &
 
 wait #1
 echo "All 3 complete"
