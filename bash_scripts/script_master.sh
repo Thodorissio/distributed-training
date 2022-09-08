23aa9864a2648ca850b1828e94376ff920da0320
jimbou <el17072@mail.ntua.gr>
2022-09-08 10:47:21 +0300

    Added comments in bash scripts where virtual env activation must occur

--- bash_scripts/script_master.sh
+++ bash_scripts/script_master.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 if [ $2 == "1" ]; then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted_training/python_scripts_$1.py
+  #activate virtual env 
+  #change to correct paths
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted-training/python_scripts_$1.py
   echo "Running script for $1 model only on master vm (who is me)"
 elif [ $1 == "2" ];
 then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted_training/python_scripts_$1.py  
+  #activate virtual env 
+  #change to correct paths
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted-training/python_scripts_$1.py  
   echo "Running script for $1 model on master vm (who is me) and on vm 1"
 elif [ $1 == "3" ];
 then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted_training/python_scripts_$1.py  
+  #activate virtual env 
+  #change to correct paths
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 0, "type": "worker"}}' python ~/distirbuted-training/python_scripts_$1.py  
   echo "Running script for $1 model on all 3 vms , including myself"
 fi
