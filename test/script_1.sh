de3a1bafa1e2fc4767a1a716eb778ab6a1b86fdb
Thodorissio <thodorissiozos@gmail.com>
2022-09-09 14:17:29 +0300

    test with ports

--- test/script_1.sh
+++ test/script_1.sh
@@ -5,10 +5,10 @@ if [ $1 == "1" ]; then
   echo "Running script for test model only on master vm , not here"
 elif [ $1 == "2" ];
 then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1"]}, "task": {"index": 1, "type": "worker"}}'/home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/test/worker.py  
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22:22", "192.168.1.1"]}, "task": {"index": 1, "type": "worker"}}'/home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/test/worker.py  
   echo "Running script for test model on master vm and on vm 1 (who is me)"
 elif [ $1 == "3" ];
 then
-  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 1, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/test/worker.py  
+  TF_CONFIG='{"cluster": {"worker": ["83.212.80.22:22", "192.168.1.1", "192.168.1.3"]}, "task": {"index": 1, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/test/worker.py  
   echo "Running script for test model on all 3 vms , including here"
 fi
