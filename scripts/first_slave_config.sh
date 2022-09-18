5a6dab416ca83324947d0823b1e10861986978aa
Thodorissio <thodorissiozos@gmail.com>
2022-09-19 00:58:42 +0300

    fix bug open port

--- scripts/first_slave_config.sh
+++ scripts/first_slave_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Configuration file for slave1
-
+fuser -k 2222/tcp
 if [ $1 == "2" ];
 then
   TF_CONFIG='{"cluster": {"worker": ["83.212.80.22:2222", "192.168.1.1:2222"]}, "task": {"index": 1, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/scripts/worker.py $1 $2
--- scripts/second_slave_config.sh
+++ scripts/second_slave_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+fuser -k 2222/tcp
 #Configuration file for slave2
 TF_CONFIG='{"cluster": {"worker": ["83.212.80.22:2222", "192.168.1.1:2222", "192.168.1.3:2222"]}, "task": {"index": 2, "type": "worker"}}' /home/user/miniconda3/envs/distributed_training/bin/python3 /home/user/distributed-training/scripts/worker.py $1 $2
